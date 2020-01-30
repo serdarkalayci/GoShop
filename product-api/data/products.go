8d94e94be5ffe782d37a7f5806305d95d634cf2f
Serdar Kalaycı <serdarkalayci@Serdars-MacBook-Pro.local>
2020-01-30 18:52:58 +0300

    Add add and update methods

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -29,8 +29,42 @@ func (p *Products) ToJson(w io.Writer) error {
 	return e.Encode(p)
 }
 
+func (p *Product) FromJson(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
+
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	fp, i, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = fp.ID
+	productList[i] = p
+	return nil
+}
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 var productList = []*Product{
--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"goshop/product-api/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -15,9 +17,69 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+	if r.Method == http.MethodPut {
+		// expect the id in the URI
+		rx := regexp.MustCompile("/([0-9]+)")
+		g := rx.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.l.Printf("ID: ", id)
+		p.updateProducts(id, rw, r)
+	}
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJson(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+		return
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	product := &data.Product{}
+	err := product.FromJson(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+	p.l.Printf("Product: %#v", product)
+	data.AddProduct(product)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	product := &data.Product{}
+	err := product.FromJson(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+	err = data.UpdateProduct(id, product)
+	if err != nil {
+		http.Error(rw, "Unable to update product json", http.StatusInternalServerError)
+		return
+	}
+	p.l.Printf("Product: %#v", product)
 }
